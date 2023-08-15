dfd2cfe31598e8258af54a03e581554edb4e688a
abelRdimby <40042hiuminess@gmail.com>
2023-08-15 16:06:57 +0200

    dataUser s'affiche dans le console

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 const form = document.getElementById("form-user-add")
-const firstname = document.getElementById("firstname").value
-const lastname = document.getElementById("lastname").value
-const email = document.getElementById("email").value
-const biographie = document.getElementById("biographie").value
-const sex = document.getElementById("sex").value
-const birthday = document.getElementById("birthday").value
+let firstname = document.getElementById("firstname")
+let lastname = document.getElementById("lastname")
+let email = document.getElementById("email")
+const biographie = document.getElementById("biographie")
+const sex = document.getElementById("sex")
+const birthday = document.getElementById("birthday")
 
 form.addEventListener('submit',(e) =>{
     e.preventDefault()
-    console.log("alatsao")
+    let dataUser = {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        email: email.value,
+        biographie: biographie.value,
+        sex: sex.value,
+        birthday: birthday.value
+    }
+    console.log(dataUser)
 });
