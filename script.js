86a226f5eab6e7e79501ede7aa3c86521e3d82f6
abelRdimby <40042hiuminess@gmail.com>
2023-08-16 09:27:57 +0200

    validation alright

--- script.js
+++ script.js
@@ -8,6 +8,32 @@ const birthday = document.getElementById("birthday")
 
 form.addEventListener('submit',(e) =>{
     e.preventDefault()
+
+    if(firstname.value === "") {
+        alert("le champ est vide");
+        return;
+    }
+    
+    if(lastname.value ==="") {
+        alert("Le champ prénom est obligatoir")
+        return;
+    }
+    if(email.value ==="") {
+        alert("L'email est obligatoir")
+        return;
+    }
+    if(biographie.value ==="") {
+        alert("On a besoin de votre biographie")
+        return;
+    }
+    if(birthday.value ==="") {
+        alert("Veuillez entré votre date naissance")
+        return;
+    }
+    if(sex.value ==="none") {
+        alert("Veuillez entré votre date naissance")
+        return;
+    }
     let dataUser = {
         firstname: firstname.value,
         lastname: lastname.value,
@@ -16,6 +42,6 @@ form.addEventListener('submit',(e) =>{
         sex: sex.value,
         birthday: birthday.value
     }
-    console.log("Bienvenue "+dataUser.firstname)
     console.log(dataUser)
 });
+
