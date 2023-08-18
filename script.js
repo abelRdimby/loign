4bf65483e3c0822f1e1ffa8b6b160329380642f8
abelRdimby <40042hiuminess@gmail.com>
2023-08-18 13:56:52 +0200

    data is geting to edit

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
-// window.location.reload()
+const dataBaseLink = ("http://localhost:3000/users");
+
+fetch(dataBaseLink)
+    .then((dataResponse) => dataResponse.json())
+    .then (data => {
+        data.forEach(function (element, index, array){
+            const usersId = data[index].id; 
+            
+            document.getElementById('tableBody').innerHTML += `
+                <tr>
+                        <th style="text-align: left"><p>${data[index].lastname}</p></th>
+                        <th style="text-align: left"><p>${data[index].firstname}</p></th>
+                        <th style="text-align: left"><p>${data[index].email}</p></th>
+                    <div >
+                        <svg xmlns="http://www.w3.org/2000/svg" onclick="editUser(${usersId})" data-id="${usersId}" class="btnUserEdit icon icon-tabler icon-tabler-edit" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                            <path d="M7 7h-1a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-1"></path>
+                            <path d="M20.385 6.585a2.1 2.1 0 0 0 -2.97 -2.97l-8.415 8.385v3h3l8.385 -8.415z"></path>
+                            <path d="M16 5l3 3"></path>
+                        </svg>
+                    </div>
+                </tr>
+            ` 
+        });    
+    }) 
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    });
+
+function editUser(usersId) {
+    fetch(`http://localhost:3000/users/${usersId}`)
+    .then((dataResponse) => dataResponse.json())
+    .then (data => {
+        console.log(data)        
+    }) 
+    .catch(error => {
+        console.error('Error fetching data:', error);
+    }); 
+}
+
 const form = document.getElementById("form-user-add")
-let firstname = document.getElementById("firstname")
-let lastname = document.getElementById("lastname")
-let email = document.getElementById("email")
+const firstname = document.getElementById("firstname")
+const lastname = document.getElementById("lastname")
+const email = document.getElementById("email")
 const biographie = document.getElementById("biographie")
 const sex = document.getElementById("sex")
 const birthday = document.getElementById("birthday")
-
 
 function notValidAnim(className , inputId ,message) {
     let Inputparent = document.getElementsByClassName(className)[0]
@@ -14,10 +52,10 @@ function notValidAnim(className , inputId ,message) {
        
     var input = document.getElementsByName(inputId)[0]
     input.setAttribute('placeholder', message)
-    input.style.border = "red 1px solid"
+    input.style.border = "var(--color-red) 1px solid"
     input.classList.add('invalid-input')
     Inputparent.style.transition="0.9s all"
-    setTimeout(() =>{
+    setTimeout(() => {
         Inputparent.classList.remove('animation');
         input.classList.remove('invalid-input')
         input.style.border = "rgb(211, 211, 211) 1px solid"
@@ -62,22 +100,26 @@ form.addEventListener('submit',(e) =>{
     if(sex.value ==="none") {
         // alert("Veuillez entré votre date naissance")
         var inputSelect = document.getElementById("sex")
-        inputSelect.style.border = "red 1px solid"
+        inputSelect.style.border = "var(--color-red) 1px solid"
         notValidAnim("sexWraper","sex")
         return;
     }
 
-    let dataUser = {
-        firstname: firstname.value,
-        lastname: lastname.value,
-        email: email.value,
-        biographie: biographie.value,
-        sex: sex.value,
-        birthday: birthday.value
-    }
-        console.log(dataUser)
-    
+// POST METHOD
+    const postLink = ("http://localhost:3000/users")
+    fetch(postLink, {
+        method: 'POST',
+        body: JSON.stringify({
+            firstname: firstname.value,
+            lastname: lastname.value,
+            email: email.value,
+            biographie: biographie.value,
+            sex: sex.value,
+            birthday: birthday.value
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
 
 });
-
-
---
+++ src/abv/mainAxiosUSe.js
@@ -0,0 +1,89 @@
+// import axios from 'axios';
+
+getData();
+function getData() {
+    axios.get('http://localhost:3000/users')
+ .then((response) => {
+    response.data.forEach(function (element, index, array){
+        // console.log(element)
+        document.getElementById('tableau').innerHTML += `
+        <tr>
+            <th style="text-align: left"><p>${element.name}</p></th>
+            <th style="text-align: left"><p>${element.username}</p></th>
+            <th style="text-align: left"><p>${element.email}</p></th>
+        </tr>
+        `
+    });
+ })
+ .catch((error) => console.log(error));
+}
+const block = document.querySelector('#block')
+const App = document.getElementById('myApp')
+const modifBtn = document.getElementById('modif')
+function removeElement() {
+    block.remove()  
+    addNewBlock()
+    console.log('voafafa')
+}
+
+function createElement() {
+    const myBlock = document.createElement('div')
+    myBlock.classList.add('userBlock')
+    document.append.append(myBlock)
+    console.log('misia')
+}
+modifBtn.addEventListener('click',createElement)
+function addNewBlock() {
+    const title = document.getElementById('myTitle').innerHTML ='Add a new users ?'
+    document.getElementById('myApp').innerHTML += `
+        <div class="useradd" id="signUp" style="transition: all 0.9s;">
+            <form class="myForm" id="myForm" method="POST">
+                <input class="inputValue" type="text" id="name" placeholder="Your name">
+                <input class="inputValue" type="text" id="username" placeholder="Your username">
+                <input class="inputValue" type="email" id="email" placeholder="Your email">
+                <input type="submit" value="submit">
+            </form>
+        </div>
+     ` 
+}
+const addUserBtn = document.getElementById('addUserBtn')
+addUserBtn.addEventListener('click', function(){
+    removeElement()
+});
+
+
+let nameInput =document.getElementById('name')
+let usernameInput =document.getElementById('username')
+let emailInput =document.getElementById('email')
+let addUserForms = document.getElementById("myForm");
+
+addUserForms.addEventListener('submit', function(e) {
+    e.preventDefault();
+    sendData();
+    console.log('coucou');
+
+    const sendData = (data) =>{
+        axios.post('http://localhost:3000/users', data)
+        .then(response =>{
+            console.log(response)   
+        })
+        .catch(error =>{
+            console.log(error)
+            getData()
+            createElement()
+        })
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
